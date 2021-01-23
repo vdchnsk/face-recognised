c7a3f349e778f703953be9499b394b968caca7eb
Valeriy Dichanskiy <dichansky@gmail.com>
2021-01-23 21:21:21 +0300

    now the window with the asset or cam works

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import cv2
 
-print("123")
+#Face finder
+face_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+#Webcome grabbing
+asset  = cv2.VideoCapture("assets/Can You Watch This Without Smiling_.mp4")
+
+#Showing the asset
+while True:
+    #reads the current frame of the asset or the webcam stream
+    succesfully_read_frame , frame = asset.read()
+    if not succesfully_read_frame:
+        break
+    # window settingd
+    cv2.imshow("Smile detector",frame)
+
+    #Display
+    cv2.waitKey(40) #updating frames every 40 ms , if parametes are emty or == 0, then the program will be waiting for key pressing
+
+
+#Clean up
+asset.release() #OS gives us a permisiion to use camera/video-asset e.t.c (I got it so)
+cv2.destroyAllWindows() #destroyong all the recently opened smile detector windows
