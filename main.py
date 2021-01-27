9b988591ae244dd1c58dc2a37769248e8c0cf311
Valeriy Dichanskiy <dichansky@gmail.com>
2021-01-27 18:38:50 +0300

    now ,the app detects faces

--- main.py
+++ main.py
@@ -8,15 +8,23 @@ asset  = cv2.VideoCapture("assets/Can You Watch This Without Smiling_.mp4")
 
 #Showing the asset
 while True:
-    #reads the current frame of the asset or the webcam stream
-    succesfully_read_frame , frame = asset.read()
-    if not succesfully_read_frame:
+    succesfully_read_frame , frame = asset.read() #reads the current frame of the asset or the webcam stream
+    if not succesfully_read_frame: #Looks for the errors and stops the apps work if they are
         break
-    # window settingd
+
+    frame_grayscale = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY) #B&W-mode (optimization: RGB has 3 channels ,but B&W only 1)
+    
+    #Detect all the faces first
+    faces = face_detector.detectMultiScale(frame_grayscale)
+    
+    for (x, y, w, h) in faces: # taking dots from the lidt "faces"
+        cv2.rectangle(frame,(x,y),(x+w , y+h), (100,200,50), 4) # Drawing rectangle around faces (100,200,50)- color; 4- 4px of sickness of the rectangle
+
+    # window settings
     cv2.imshow("Smile detector",frame)
 
     #Display
-    cv2.waitKey(40) #updating frames every 40 ms , if parametes are emty or == 0, then the program will be waiting for key pressing
+    cv2.waitKey(1) #updating frames every 1 ms , if parametes are emty or == 0, then the program will be waiting for key pressing
 
 
 #Clean up
