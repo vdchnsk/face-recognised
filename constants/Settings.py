1f14df4003cd6137684179b740958887004f0574
Valeriy Dichanskiy <dichansky@gmail.com>
2022-01-15 18:30:18 +0300

    Added constants

---
+++ constants/Settings.py
@@ -0,0 +1,20 @@
+class WindowSettings:
+    def __init__():
+        pass
+
+    WINDOW_NAME = 'Smile detector by Duckoman'
+    WINDOW_FONT_SIZE = 3
+
+
+class KeyboardKeys:
+    def __init__():
+        pass
+
+    SPACE = 32
+
+class Images:
+    def __init__():
+        pass
+    
+    SUPPORTED_IMAGE_EXTENSIONS = ['.jpg', '.png', 'jpeg']
+
---
+++ constants/Strings.py
@@ -0,0 +1,5 @@
+class Strings:
+    def __init__():
+        pass
+
+    SMILING = 'smiling'
